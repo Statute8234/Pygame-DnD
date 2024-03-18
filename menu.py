2ed059638ff16ecf76daeee57eb506da9ed2b781
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-03-18 10:33:01 -0400

    Update menu.py
    + adding pause menu

--- menu.py
+++ menu.py
@@ -121,6 +121,106 @@ class Button:
     def reset(self):
         self.clicked = False
         self.color = self.inactive_color
+# pause menu
+class PauseMenu:
+    def __init__(self, screen):
+        self.screen = screen
+        self.title = Text("Menu", 100, BLACK, (50, 50))
+        self.buttons = [Button(50,120,200,50,"Resume",RED,BLACK),
+                        Button(50,180,200,50,"Options",RED,BLACK),
+                        Button(50,240,200,50,"Quit",RED,BLACK),]
+        self.backButton = Button(100,120,50,50,"Back",RED,BLACK)
+        self.showSettings = False
+        self.showLoadScreen = False
+        self.settings()
+        self.loadScreen()
+    
+    def loadScreen(self):
+        self.loadSlots = [Button(50, (x * 60) + 200, 200, 50, "Slot", RED, BLACK) for x in range(5)]
+        
+    def settings(self):
+        self.sound = Text("Music", 36, BLACK, (230, 200))
+        self.sound_slider = Slider(self.screen, 230, 240, 200, 10, min=0, max=100, step=1, initial=100)
+        self.sound_output = TextBox(self.screen, 440, 210, 50, 50, fontSize=25)
+        self.sound_output.disable()
+        self.sound_effects = Text("Sound Effects", 36, BLACK, (230, 300))
+        self.sound_effects_slider = Slider(self.screen, 230, 340, 200, 10, min=0, max=100, step=1, initial=100)
+        self.sound_effects_output = TextBox(self.screen, 440, 310, 50, 50, fontSize=25)
+        self.sound_effects_output.disable()
+        self.frame_rate = Text("Frame Rate", 36, BLACK, (230, 400))
+        self.frame_rate_slider = Slider(self.screen, 230, 450, 128, 10, min=1, max=64, step=1, initial=64)
+        self.frame_rate_output = TextBox(self.screen, 440, 413, 50, 50, fontSize=25)
+        self.frame_rate_output.disable()
+        self.brightness_text = Text("Brightness", 36, BLACK, (230, 500))
+        self.brightness_slider = Slider(self.screen, 230, 550, 200, 10, min=0, max=1.0, step=0.1, initial=1.0)
+        self.brightness_output = TextBox(self.screen, 440, 513, 50, 50, fontSize=25)
+        self.brightness_output.disable()
+
+    def updateData(self,Background_volume,soundEffect_volume,frameRate,brightness):
+        Background_volume = self.sound_slider.getValue()
+        soundEffect_volume = self.sound_effects_slider.getValue()
+        frameRate = self.frame_rate_slider.getValue()
+        brightness = round(self.brightness_slider.getValue(),2)
+        return [Background_volume,soundEffect_volume,frameRate,brightness]
+
+    def draw(self):
+        self.screen.fill(WHITE)
+        self.title.render(self.screen)
+        if self.showSettings == False and self.showLoadScreen == False:
+            for button in self.buttons:
+                button.draw(self.screen)
+        # settings
+        if (self.showSettings):
+            self.backButton.draw(self.screen)
+            self.sound.render(self.screen)
+            self.sound_slider.draw()
+            self.sound_output.draw()
+            self.sound_output.setText(self.sound_slider.getValue())
+
+            self.sound_effects.render(self.screen)
+            self.sound_effects_slider.draw()
+            self.sound_effects_output.draw()
+            self.sound_effects_output.setText(self.sound_effects_slider.getValue())
+
+            self.frame_rate.render(self.screen)
+            self.frame_rate_slider.draw()
+            self.frame_rate_output.draw()
+            self.frame_rate_output.setText(self.frame_rate_slider.getValue())
+
+            self.brightness_text.render(self.screen)
+            self.brightness_slider.draw()
+            self.brightness_output.draw()
+            self.brightness_output.setText(round(self.brightness_slider.getValue(),2))
+    
+    def update(self, screenHeight, screenWidth):
+        self.title.position = ((screenHeight / 2) - 100, 50)
+
+        button_x = (screenHeight / 2) - 100
+        for button in self.buttons:
+            button.x = button_x
+
+    def handle_event(self, event):
+        for button in self.buttons:
+            button.handle_event(event)
+            if button.color == button.active_color:
+                button.textColor = button.active_color
+            else:
+                button.textColor = button.inactive_color
+            if button.clicked:
+                button.reset()
+                if button.text == "Options":
+                    self.showSettings = True
+                else:
+                    return button.text
+            # back button
+            pygame_widgets.update(event)
+            self.backButton.handle_event(event)
+            if self.backButton.clicked:
+                if self.showSettings:
+                    self.showSettings = False
+                else:
+                    self.showLoadScreen = False
+                self.backButton.reset()
 # player sheet
 class PlayerSheet:
     def __init__(self, screen):

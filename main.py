244418bce4f18b6375e656e5acbf2b21a918a9c8
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-03-18 10:32:16 -0400

    Update main.py
    - adding pause menu
    + might need to fix options [fix the values when changed]

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ BLACK = (0,0,0)
 # menus
 mainMenu = menu.Menu(screen)
 playerSheet = menu.PlayerSheet(screen)
+pauseMenu = menu.PauseMenu(screen)
 # display functions
 brightness = 1.0
 frameRate = 64
@@ -36,11 +37,12 @@ def adjust_brightness(surface, brightness):
     overlay.set_alpha(255 - int(brightness * 255))
     surface.blit(overlay, (0, 0))
 # loop
+show_pauseMenu = False
 show_playerSheet = False
 show_mainmenu = True
 running = True
 def main():
-    global screenWidth, screenHeight, running, show_mainmenu, show_playerSheet, Background_volume,soundEffect_volume,frameRate,brightness
+    global screenWidth, screenHeight, running, show_mainmenu, show_playerSheet, show_pauseMenu, Background_volume,soundEffect_volume,frameRate,brightness
     while running:
         events = pygame.event.get()
         for event in events:
@@ -49,9 +51,7 @@ def main():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE and show_mainmenu == False:
-                    show_mainmenu = not(show_mainmenu)
-                    show_playerSheet = not(show_playerSheet)
-                if event.key == pygame.K_e and show_mainmenu == False:
+                    show_pauseMenu = not(show_pauseMenu)
                     show_playerSheet = not(show_playerSheet)
             elif event.type == pygame.VIDEORESIZE:
                 screenWidth, screenHeight = event.size
@@ -64,6 +64,16 @@ def main():
                 if mainMenu.handle_event(event) == "New Game" or mainMenu.handle_event(event) == "Continue":
                     show_mainmenu = False
                     show_playerSheet = True
+            # pause menu
+            if show_pauseMenu:
+                pauseMenu.handle_event(event)
+                if pauseMenu.handle_event(event) == "Resume":
+                    show_pauseMenu = False
+                    show_playerSheet = False
+                if pauseMenu.handle_event(event) == "Quit":
+                    show_pauseMenu = False
+                    show_mainmenu = True
+                    show_playerSheet = False
         # player sheet
         if show_playerSheet:
             playerSheet.handle_event(events)
@@ -75,6 +85,10 @@ def main():
             mainMenu.draw()
         if show_playerSheet:
             playerSheet.draw()
+        if show_pauseMenu:
+            Background_volume,soundEffect_volume,frameRate,brightness = pauseMenu.updateData(Background_volume, soundEffect_volume, frameRate, brightness)
+            pauseMenu.update(screenWidth, screenHeight)
+            pauseMenu.draw()
         # update
         adjust_brightness(screen, brightness)
         pygame.display.flip()
